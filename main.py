ddba3380324d9ff3a59a01a11d7ee8927e317f95
Oshada Dilshan <dilshan-se20012@stu.kln.ac.lk>
2024-11-01 23:55:24 +0530

    Finished basic structure

--- main.py
+++ main.py
@@ -27,12 +27,34 @@ def main():
     init()
 
     st.header("Ask from Prince Vlad")
-    
+
     with st.sidebar:
         user_input = st.text_input("Your Message: ",key="user_input")
 
     if user_input:
-        message(user_input, is_user=True)
+
+        hf_api_token = os.getenv("HUGGINGFACE_API_KEY")
+        model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+        
+        llm = HuggingFaceHub(
+            repo_id=model_name,
+            huggingfacehub_api_token=hf_api_token
+        )
+
+        prompt = PromptTemplate(
+            input_variables=["user_input"],
+            template=" {user_input}\n"
+        )
+
+        chat_chain = LLMChain(
+            llm=llm,
+            prompt=prompt
+        )
+
+        with st.spinner("Thinking...."):
+            response = chat_chain.run(user_input=user_input)
+        message.message(user_input, is_user=True)
+        message.message("Prince Vlad: " + response, is_user=False)
 
 if __name__ == "__main__":
     main()
