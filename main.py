d90a2caa990b7b42f51f22f0e4b2313429691f35
Oshada Dilshan <dilshan-se20012@stu.kln.ac.lk>
2024-11-01 21:41:34 +0530

    creating prompts and templates

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ from dotenv import load_dotenv
 import os
 
 from langchain_community.llms import HuggingFaceHub
-#from langchain.chat_models import ChatModel
-from langchain_community.chat_models import ChatModel
-
-from langchain.schemas import (
-      SystemMessage,
-      HumanMessage,
-      AIMessage
-)
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain  # Use LLMChain for chaining prompts and models
 
 def init():
     load_dotenv()
@@ -25,16 +19,20 @@ def init():
 
     st.set_page_config(
             page_title= "My chat bot",
-            page_icon="</>"
+            page_icon="🤖"
     )
 
 
 def main():
     init()
-   
+
     st.header("Ask from Prince Vlad")
+    
     with st.sidebar:
         user_input = st.text_input("Your Message: ",key="user_input")
+
+    if user_input:
+        message(user_input, is_user=True)
 
 if __name__ == "__main__":
     main()
